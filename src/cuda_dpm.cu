a965eab260a50471f77e7e176d5bc329131e3110
Andrew Ton <atatat123@gmail.com>
2022-12-12 14:43:52 -0500

    replacing with address in memcpytosymbol

--- src/cuda_dpm.cu
+++ src/cuda_dpm.cu
@@ -2468,10 +2468,10 @@ void dpm::setDeviceVariables() {
   // set device variables needed for force kernel
   double rho0 = sqrt(a0.at(0));
   cout << "before setting device variables: d_numVertices = " << d_numVertices << ", d_kc = " << d_kc << '\n';
-  cudaMemcpyToSymbol(d_numVertices, &(NVTOT), sizeof(NVTOT));
-  cudaMemcpyToSymbol(d_L, &L[0], 2 * sizeof(double));
-  cudaMemcpyToSymbol(d_rho0, &(rho0), sizeof(rho0));
-  cudaMemcpyToSymbol(d_kc, &(kc), sizeof(kc));
+  cudaMemcpyToSymbol(&d_numVertices, &(NVTOT), sizeof(NVTOT));
+  cudaMemcpyToSymbol(&d_L[0], &L[0], 2 * sizeof(double));
+  cudaMemcpyToSymbol(&d_rho0, &(rho0), sizeof(rho0));
+  cudaMemcpyToSymbol(&d_kc, &(kc), sizeof(kc));
   cout << "after setting device variables: d_numVertices = " << d_numVertices << ", d_kc = " << d_kc << '\n';
 }
 
