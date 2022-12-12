a86eb4ae9b5d3d06f1bacdecd44cfc0cb713488e
Andrew Ton <atatat123@gmail.com>
2022-12-12 10:47:03 -0500

    switching to cuda kernel for main calculation

--- main/test/cudaNVE.cu
+++ main/test/cudaNVE.cu
@@ -64,10 +64,12 @@ int main() {
   int NPRINTSKIP = (int)floor(tskip / dt0);*/
   int NT = 100;
   int NPRINTSKIP = 10;
-  configobj2D.vertexNVE2D(enout, forceUpdate, T, dt0, NT, NPRINTSKIP);
+  // configobj2D.vertexNVE2D(enout, forceUpdate, T, dt0, NT, NPRINTSKIP);
+
+  configobj2D.cudaVertexNVE(enout, T, dt0, NT, NPRINTSKIP);
 
   // say goodbye
-  cout << "\n\n** Finished dpmNVEtest.cpp, ending. " << endl;
+  cout << "\n\n** Finished cudaNVE.cpp, ending. " << endl;
 
   // close file
   enout.close();
